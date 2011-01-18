584aa007c12a9bd3bd5c2e735d0083b516fe87f9
Adrien Lemaire <lemaire.adrien@gmail.com>
2011-01-18 18:38:48 +0000

    bug solved.. media/tex is mandatory, otherwise the pdf generation fail

--- settings.py
+++ settings.py
@@ -22,7 +22,6 @@ DATABASES = {
 }
 
 HOME = os.path.dirname(__file__)
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -48,12 +47,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(HOME, "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = os.path.join(HOME, "media")
+MEDIA_URL = ''
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -96,7 +95,7 @@ INSTALLED_APPS = (
     'latex',
 )
 
-TEX_URL = os.path.join(MEDIA_URL, "tex")
+TEX_URL = os.path.join(MEDIA_ROOT, "tex")
 DEBUG_PDF = False
 
 try:
